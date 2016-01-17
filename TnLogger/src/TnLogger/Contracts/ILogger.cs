32d4585d823cb8d73fd41d82ced8bdb774addb91
reza bazargan <reza_bazargan@msn.com>
2016-01-17 11:02:38 +0330

    ILogger Added

---
+++ TnLogger/src/TnLogger/Contracts/ILogger.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TnLogger.Contracts
+{
+    public interface ILogger
+    {
+        Guid Id { get; set; }
+        string Title { get; set; }
+        string Description { get; set; }
+        DateTime CreateDate { get; set; }
+        string User { get; set; }
+
+        LogType LogType { get; set; }
+    }
+}
